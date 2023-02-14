f0dd7482625f30b32b620c50634c29ee90f0a30d
ydvrahul19 <85828386+ydvrahul19@users.noreply.github.com>
2023-02-14 17:49:57 +0000

    init

---
+++ app/src/App.js
@@ -0,0 +1,18 @@
+import React, { useRef } from "react";
+
+function App() {
+  const inputRef = useRef(null);
+
+  const handleClick = () => {
+    inputRef.current.focus();
+  };
+
+  return (
+    <div>
+      <input type="text" ref={inputRef} />
+      <button onClick={handleClick}>Focus Input</button>
+    </div>
+  );
+}
+
+export default App;

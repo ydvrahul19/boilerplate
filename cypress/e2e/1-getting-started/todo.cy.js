fb5113c292e7f4cc1eaa604d5e06a02ab9da21f0
ydvrahul19 <85828386+ydvrahul19@users.noreply.github.com>
2023-02-15 11:20:57 +0530

    Update todo.cy.js

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -5,4 +5,15 @@ describe("UseRef hook test", () => {
     cy.get("button").click();
     cy.get("input").should("be.focused");
   });
+
+  it("displays correct button text", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("button").should("contain", "Focus Input");
+  });
+
+  it("updates input value on type", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("input").type("Hello World");
+    cy.get("input").should("have.value", "Hello World");
+  });
 });
